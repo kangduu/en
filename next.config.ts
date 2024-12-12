56504bf8d2c0f429852900b96525dbc9fcc64188
dukang <dk@nosugartech.com>
2024-12-12 11:51:16 +0800

    feat: move into src directory.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false, // log 两次
+  //
+  // output: "export",
 };
 
 export default nextConfig;
