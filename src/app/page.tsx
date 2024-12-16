98d2284ad6cb91bb1099ca383564164ea0a0173d
dukang <dk@nosugartech.com>
2024-12-16 17:58:18 +0800

    feat: history list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
-import { Link } from "@nextui-org/react";
+import { Link, Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div className="w-1/2 m-auto mt-4 bg-amber-100 px-4 py-4 rounded">
-      <div className="w-full mb-4 flex gap-4">
-        <Link href="/about">About</Link>
-        <Link href="/report">Report</Link>
-      </div>
+    <div className="w-full p-4">
+      <Button type="button" color="primary">
+        <Link href="/result" className="text-white">
+          History Results
+        </Link>
+      </Button>
     </div>
   );
 }
