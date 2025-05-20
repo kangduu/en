46b6cc21a0a1d551c8939da75433e6ec66dd8a86
dukang <dk@nosugartech.com>
2025-05-20 17:59:36 +0800

    refactor: delete button component

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,5 +1,4 @@
 export { default as Card } from "./Card";
 export { default as Spin } from "./Spin";
-export { default as Button } from "./Button";
 export { default as GridLayout } from "./GridLayout";
 export { default as Collapse } from "./Collapse";
