25eadf46c63d706e430864bec82c3bc54bc11a28
dukang <dk@nosugartech.com>
2025-05-20 11:45:58 +0800

    feat: new navigation

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,47 +1,34 @@
 "use client";
 
-import React from "react";
-
+import React, { useState } from "react";
 import Link from "next/link";
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import DarkModel from "./dark";
 import { HeaderLinks } from "../routes";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
-
-const GitHubSvg = () => (
-  <svg
-    height="20"
-    viewBox="0 0 24 24"
-    width="20"
-    aria-hidden="true"
-    focusable="false"
-    tabIndex={-1}
-  >
-    <path
-      clipRule="evenodd"
-      d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"
-      fill="currentColor"
-      fillRule="evenodd"
-    ></path>
-  </svg>
-);
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import { Github, HamburgerButton } from "@icon-park/react";
 
 export default function Navigation() {
   return (
-    <header className="w-full h-fit py-4 border-b-1">
+    <header className="w-full h-fit py-4 md:py-2 border-b-1">
       <div className="max-w-[1024px] mx-auto flex items-center justify-between px-4">
-        <Link
-          className="font-bold text-inherit mr-auto"
-          color="foreground"
-          href="/"
-        >
+        {/* 移动导航 (隐藏于桌面端) */}
+        <div className="md:hidden flex items-center mr-2">
+          <NativeNavMenu />
+        </div>
+
+        {/* Home */}
+        <Link className="mr-auto font-bold leading-none" href="/">
           ENGLISH
         </Link>
 
@@ -50,46 +37,106 @@ export default function Navigation() {
           <NavMenu />
         </div>
 
-        {/* 移动导航 (隐藏于桌面端) */}
-        <div className="md:hidden block">
-          <NativeNavMenu />
-        </div>
-
-        {/* github library */}
-        <span className="ml-auto mr-4">
+        {/* dark mode */}
+        <span className="ml-auto mr-3 cursor-pointer">
           <DarkModel />
         </span>
+
+        {/* github library */}
         <Link href="https://github.com/kangduu/en" target="_blank">
-          <GitHubSvg />
+          <Github />
         </Link>
       </div>
     </header>
   );
 }
 
+// mobile menu
 function NativeNavMenu() {
-  return null;
+  const [open, setOpen] = useState(false);
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger>
+        <HamburgerButton onClick={() => setOpen(true)} />
+      </DrawerTrigger>
+      <DrawerContent className="p-4 pt-0 min-h-[74vh]">
+        {HeaderLinks.map(({ id, title, url, children }) => {
+          return (
+            <Link
+              className="mt-2 font-bold"
+              key={id}
+              href={url}
+              onClick={() => setOpen(false)}
+            >
+              {title}
+              {children && (
+                <div>
+                  {children?.map(({ id, title, url }) => {
+                    return (
+                      <Link
+                        className="block font-normal mt-1 pl-4"
+                        key={id}
+                        href={url}
+                      >
+                        {title}
+                      </Link>
+                    );
+                  })}
+                </div>
+              )}
+            </Link>
+          );
+        })}
+      </DrawerContent>
+    </Drawer>
+  );
 }
 
+// default menu
 function NavMenu() {
   const pathname = usePathname();
+  const router = useRouter();
   return (
     <NavigationMenu>
       <NavigationMenuList>
-        {HeaderLinks.map(({ id, title, url }) => {
+        {HeaderLinks.map(({ id, title, url, children }) => {
           const isActive = url === pathname;
           return (
             <NavigationMenuItem key={id}>
-              <Link href={url} passHref>
-                <NavigationMenuLink
-                  className={cn(
-                    navigationMenuTriggerStyle(),
-                    isActive ? "text-white" : ""
-                  )}
-                >
-                  {title}
-                </NavigationMenuLink>
-              </Link>
+              {children && children.length > 0 ? (
+                <>
+                  <NavigationMenuTrigger
+                    className="cursor-pointer"
+                    onClick={() => router.push(url)}
+                  >
+                    {title}
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    {children?.map(({ id, title, url }) => {
+                      return (
+                        <Link
+                          className="block font-normal mt-1 whitespace-nowrap hover:underline"
+                          key={id}
+                          href={url}
+                        >
+                          {title}
+                        </Link>
+                      );
+                    })}
+                  </NavigationMenuContent>
+                </>
+              ) : (
+                <Link href={url} passHref>
+                  <NavigationMenuLink
+                    className={cn(
+                      navigationMenuTriggerStyle(),
+                      isActive ? "text-white" : ""
+                    )}
+                  >
+                    {title}
+                  </NavigationMenuLink>
+                </Link>
+              )}
             </NavigationMenuItem>
           );
         })}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import Navigation from "./header";
-import { Back2Top } from "../components";
-import { Toaster } from "@/components/ui/sonner";
+import Main from "./main";
 
 export const metadata: Metadata = {
   title: "English",
@@ -18,14 +15,9 @@ export default function RootLayout({
     <html lang="en" className="light">
       <body
         role="contentinfo"
-        className={`antialiased bg-white dark:bg-slate-900 dark:text-white`}
+        className={`antialiased bg-white dark:bg-[var(--background)] dark:text-white text-base`}
       >
-        <Navigation />
-        <main className="px-4 pt-4 pb-6 w-full m-auto max-w-[1024px]">
-          {children}
-          <Back2Top />
-        </main>
-        <Toaster />
+        <Main>{children}</Main>
       </body>
     </html>
   );
