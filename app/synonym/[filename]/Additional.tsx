9d851dcc19c62550ac25915be2fcb6d035e82e3e
dukang <dk@nosugartech.com>
2025-06-09 16:49:00 +0800

    fix/synonym: text color error on dark

--- app/synonym/[filename]/Additional.tsx
+++ app/synonym/[filename]/Additional.tsx
@@ -1,3 +1,4 @@
+import Empty from "@/components/svg/Empty";
 import { Separator } from "@/components/ui/separator";
 import type { Synonym } from "@/lib/actions";
 import React from "react";
@@ -17,10 +18,8 @@ function RenderObjectValue({
 }) {
   return Object.entries(value).map(([subKey, subValue], subIndex) => (
     <div key={subIndex} className="ml-4">
-      <span className="text-gray-800 font-medium mr-4">
-        {transform(subKey, false)}:
-      </span>
-      <span className="text-gray-700"> {subValue}</span>
+      <span className="font-medium mr-4">{transform(subKey, false)}:</span>
+      <span> {subValue}</span>
     </div>
   ));
 }
@@ -33,7 +32,7 @@ function RenderArrayValue({
   return (
     <ul className="list-disc pl-5">
       {value.map((item, index) => (
-        <li key={index} className="text-sm text-gray-700">
+        <li key={index} className="text-sm  ">
           {item.toString()}
         </li>
       ))}
@@ -54,13 +53,15 @@ export default function Additional({ notes }: AdditionalProps) {
       {extended_words?.length ? (
         <ul className="list-disc pl-5">
           {extended_words.map((word, index) => (
-            <li key={index} className="text-sm text-gray-700">
+            <li key={index} className="text-sm">
               {word}
             </li>
           ))}
         </ul>
       ) : (
-        <p className="text-sm text-gray-600">No extended words available.</p>
+        <div className="w-[50%] m-auto">
+          <Empty.page />
+        </div>
       )}
 
       {Object.keys(rest).length > 0 && (
@@ -68,13 +69,9 @@ export default function Additional({ notes }: AdditionalProps) {
           {Object.entries(rest).map(([key, value], index) => {
             return (
               <div key={index} className="mb-2">
-                <span className="text-gray-800 font-medium">
-                  {transform(key)}
-                </span>
+                <span className="font-medium">{transform(key)}</span>
 
-                {typeof value === "string" && (
-                  <div className="text-gray-700"> {value}</div>
-                )}
+                {typeof value === "string" && <div> {value}</div>}
 
                 {Array.isArray(value) && value.length > 0 ? (
                   <RenderArrayValue value={value} />
