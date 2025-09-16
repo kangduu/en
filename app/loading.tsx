5507b76a99a3b9151974d405b172defdc1b7dbce
dukang <dk@nosugartech.com>
2025-09-16 17:11:38 +0800

    feat: page loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,11 @@
+import Loading from "@/components/svg/Loading";
 import React from "react";
 
-export default function Loading() {
-  return <div>loading...</div>;
+export default function pending() {
+  return (
+    <div className="res-box max-w-3xl">
+      <div className="text-muted text-center mt-4">资源加载中，请稍候~~~</div>
+      <Loading />
+    </div>
+  );
 }
