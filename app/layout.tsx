efda1f05df89ad1668467621df51f0d6bd807c7b
dukang <dk@nosugartech.com>
2025-09-17 16:26:08 +0800

    fix(metadata): description

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import { SiteTitle } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: SiteTitle,
-  description: "english, en, 英语, 自学, bbc, BBC, kangduu, dukang, personal blog, nce,",
+  description:
+    "english, en, 英语, 自学, bbc, BBC, kangduu, dukang, personal blog, nce,",
 };
 
 export default function RootLayout({
