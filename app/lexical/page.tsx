5bfbac20599196c1bc1036d801d9194bef38c869
dukang <dk@nosugartech.com>
2025-09-17 17:29:39 +0800

    del: delete subhead component

--- app/lexical/page.tsx
+++ app/lexical/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import Subhead from "@/components/Subhead";
 import LexicalComp from "@/components/Lexical";
 
 export default function Lexical() {
   return (
     <>
-      <Subhead>words</Subhead>
       <LexicalComp />
     </>
   );
