dffbb5146ef508e6aeb9d447e330ae92350ab27d
dukang <dk@nosugartech.com>
2025-09-24 18:01:58 +0800

    feat: posts page

--- app/[posts]/page.tsx
+++ app/[posts]/page.tsx
@@ -2,46 +2,65 @@ import Link from "next/link";
 import { getAllSlugsByFolder } from "@/lib/markdown";
 import { Chapter } from "@/components/kit";
 import type { PropsWithChildren } from "react";
+import Empty from "@/components/svg/Empty";
+import dayjs from "dayjs";
+
+const Folders = ["blog", "nce-course", "bbc"] as const;
+type PostsFolder = (typeof Folders)[number];
 
 const View = async ({
   children,
   folder,
-}: PropsWithChildren & { folder: string }) => {
+}: PropsWithChildren & { folder: PostsFolder }) => {
   const posts = await getAllSlugsByFolder(folder);
   if (!posts?.length) return null;
   return (
     <section className="mb-4">
       {children}
       <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={`/${folder}/${post.slug}`}>{post.title}</Link>
-            {post.date && <span> — {post.date}</span>}
-          </li>
-        ))}
+        {posts.map((post) => {
+          return (
+            <li key={post.slug}>
+              <Link href={`/${folder}/${post.slug}`}>{post.title}</Link>
+              {post.date && <span> — {dayjs(post.date).format("YYYY-MM-DD HH:mm")}</span>}
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
 };
 
 type Props = {
-  params: Promise<{ posts: string }>;
+  params: Promise<{ posts: PostsFolder }>;
 };
 
-// 笔记列表页面
+/**
+ * 笔记博客页面
+ * @desc 可根据 lib/posts 目录展示不同的页面内容
+ * @param param0
+ * @returns
+ */
 export default async function NoteListPage({ params }: Props) {
   const { posts } = await params;
+  if (!Folders.includes(posts)) return <Empty.page />;
   return (
     <main className="res-box">
-      <View folder="blog">
-        <Chapter title="我的笔记" desc="学习笔记，好记性不如烂笔头！" />
-      </View>
-      <View folder="nce-course">
-        <Chapter title="新概念课堂笔记" desc="todo" />
-      </View>
-      <View folder="bbc">
-        <Chapter title="BBC短语学习" desc="todo" />
-      </View>
+      {posts === "blog" && (
+        <View folder="blog">
+          <Chapter title="我的笔记" desc="学习笔记，好记性不如烂笔头！" />
+        </View>
+      )}
+      {posts === "nce-course" && (
+        <View folder="nce-course">
+          <Chapter title="新概念课堂笔记" desc="todo" />
+        </View>
+      )}
+      {posts === "bbc" && (
+        <View folder="bbc">
+          <Chapter title="BBC短语学习" desc="todo" />
+        </View>
+      )}
     </main>
   );
 }
