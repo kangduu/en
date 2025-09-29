280fae27634f3efe4c195828a6db38d21f00afc1
dukang <dk@nosugartech.com>
2025-09-29 15:34:36 +0800

    fix: utterance

--- lib/utterance.ts
+++ lib/utterance.ts
@@ -1,14 +1,15 @@
 export default function utterance(
   text: string,
   option?: {
-    lang?: SpeechSynthesisVoice | null;
+    lang?: string;
+    voice?: SpeechSynthesisVoice | null;
     rate?: number; // 语速
     pitch?: number; // 音调
   }
 ): () => void {
   const utterance = new SpeechSynthesisUtterance(text);
-  utterance.lang = "en-US";
-  utterance.voice = option?.lang || null;
+  utterance.lang = option?.lang || "en-US";
+  utterance.voice = option?.voice || null;
   utterance.rate = option?.rate || 1;
   utterance.pitch = option?.pitch || 1;
 
